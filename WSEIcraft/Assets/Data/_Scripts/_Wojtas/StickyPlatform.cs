d39369893280017ed5016d05a71715c114e3abea
PanZenon <karolwojtas2005@gmail.com>
2023-03-31 20:56:17 +0200

    Oro

---
+++ WSEIcraft/Assets/Data/_Scripts/_Wojtas/StickyPlatform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
