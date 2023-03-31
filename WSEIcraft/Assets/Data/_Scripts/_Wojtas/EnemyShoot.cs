fa51661cde33ea6c0a2b058a3beab1a084d81d9f
PanZenon <karolwojtas2005@gmail.com>
2023-03-31 21:02:11 +0200

    check

--- WSEIcraft/Assets/Data/_Scripts/_Wojtas/EnemyShoot.cs
+++ WSEIcraft/Assets/Data/_Scripts/_Wojtas/EnemyShoot.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyShoot : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
+    float currentxPosition;
+    float currentyPosition;
     void Start()
     {
         StartCoroutine(Shoot());
+        currentxPosition = transform.position.x;
+        currentyPosition = transform.position.y;
     }
 
     IEnumerator Shoot()
@@ -15,5 +20,9 @@ public class EnemyShoot : MonoBehaviour
         Instantiate(bullet, transform.position, transform.rotation);
         yield return new WaitForSeconds(5);
         StartCoroutine(Shoot());
+    }
+    bool ifGoingRight()
+    {
+        return currentxPosition < transform.position.x;
     }
 }
