8ac577fe6380eb09f60c6ba8900c77d2bf6f68c7
PanZenon <karolwojtas2005@gmail.com>
2023-04-01 06:36:27 +0200

    easteregg

--- WSEIcraft/Assets/Data/_Scripts/_Wojtas/QueenDied.cs
+++ WSEIcraft/Assets/Data/_Scripts/_Wojtas/QueenDied.cs
@@ -1,13 +1,19 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class QueenDied : MonoBehaviour
 {
+    [SerializeField] CinemachineVirtualCamera endCamera;
+    [SerializeField] TextMeshProUGUI endScreen;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            endScreen.text = "WSEICRAFT";
+            endCamera.Priority = 11;
             Destroy(this.gameObject);
         }
     }
