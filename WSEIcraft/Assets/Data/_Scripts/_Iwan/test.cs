b4aa076a9ca86265cd5e4ab55228788934371e3e
xAxee <50153178+xAxee@users.noreply.github.com>
2023-04-01 04:53:37 +0200

    Mario and guide

--- WSEIcraft/Assets/Data/_Scripts/_Iwan/test.cs
+++ WSEIcraft/Assets/Data/_Scripts/_Iwan/test.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class test : MonoBehaviour
 {
     public void loadNext() {
+        Debug.Log("next");
         SceneManager.LoadScene("MarioGuide2");
     }
 }
